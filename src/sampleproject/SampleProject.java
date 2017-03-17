4f9ca3c0c1953290dd818421ff76400cc60028b1
Bharath <Bharath@RajBharath>
2017-03-17 14:50:20 +0530

    Sample Project was added by Raj Bharath on 17/032017

---
+++ src/sampleproject/SampleProject.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sampleproject;
+
+
+
+/**
+ *
+ * @author Bharath
+ */
+public class SampleProject {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is Sample Project");
+    }
+    
+}
